1c1842fac946fd100686ded5d1180e81dd083c6f
Ramez Chreide <ramiz_chreide@outlook.com>
2025-03-06 09:42:59 +0100

    add: vampiro one font, gradient text

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
 
     return (
         <div className="relative z-10 p-8">
+            <h1 className="font-vampiro gradient-text">{'<R_Ch/>'}</h1>
             <Button text="DOWNLOAD CV" downloadPath="./Ramez_Chreide_2025_CV.pdf" fileName="Ramez_Chreide_CV.pdf" isPrimary={true}/>
             <Button onClick={handleClick}  text="Click Me" isPrimary={true}/>
             <Button onClick={handleClick}  text="Browse Portfolio" isPrimary={false}/>
