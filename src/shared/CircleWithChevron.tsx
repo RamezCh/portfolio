151c509141c0af819d6eeff51b639954ada108f9
Ramez Chreide <ramiz_chreide@outlook.com>
2025-03-12 12:10:11 +0100

    fix: make CircleWithChevron readable

--- src/shared/CircleWithChevron.tsx
+++ src/shared/CircleWithChevron.tsx
@@ -7,22 +7,21 @@ interface CircleWithChevronProps {
 }
 
 const CircleWithChevron: FC<CircleWithChevronProps> = ({ direction, enabled, onClick }) => {
+    const baseButtonStyle = "flex items-center justify-center w-22 h-22 border-2 border-[#D1E4FA] rounded-full transition-all duration-300";
+    const conditionButtonStyle = enabled ? "bg-gradient-to-r from-[#8D74F2] to-[#7051EF] hover:from-[#7B5FEE] hover:to-[#5A3FEC]"
+        : "bg-white cursor-not-allowed";
+    const buttonStyle = baseButtonStyle + " " + conditionButtonStyle;
+
+    const baseSpanStyle = "text-2xl transition-all duration-300";
+    const conditionSpanStyle = enabled ? "text-white" : "text-black";
+    const spanStyle = baseSpanStyle + " " + conditionSpanStyle;
+
+
     return (
-        <button
-            onClick={onClick}
-            className={`flex items-center justify-center w-22 h-22 border-2 border-[#D1E4FA] rounded-full transition-all duration-300 ${
-                enabled
-                    ? "bg-gradient-to-r from-[#8D74F2] to-[#7051EF] hover:from-[#7B5FEE] hover:to-[#5A3FEC]"
-                    : "bg-white cursor-not-allowed"
-            }`}
-        >
-      <span
-          className={`text-2xl transition-all duration-300 ${
-              enabled ? "text-white" : "text-black"
-          }`}
-      >
-        {direction === "left" ? "<" : ">"}
-      </span>
+        <button onClick={onClick} className={buttonStyle}>
+          <span className={spanStyle}>
+            {direction === "left" ? "<" : ">"}
+          </span>
         </button>
     );
 };
